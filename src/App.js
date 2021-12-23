b658b7855ea132f9b8a025c95266f6eaa9ef89b7
pablo <pablocorreia2012@gmail.com>
2021-12-23 16:56:05 -0300

    feito a primeira parta da separaçao dos componentes

--- src/App.js
+++ src/App.js
@@ -1,72 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
+import {loadPosts} from './utils/load-post' 
+import { Posts } from './components/Posts';
+
 class App extends Component{
-  // constructor(props){
-    // super(props);
-    // this.handlePClick = this.handlePClick.bind(this)
     state = {
-      name: 'Pablo Correia',
-      counter: 0
+      posts: []
     }
 
-  // }
-
-  handlePClick = () =>{
-    this.setState({name: 'paula'})
+  async componentDidMount(){
+    await this.loadPosts();
   }
 
-  handlAClick = (event) =>{
-    event.preventDefault()
-    const {counter} = this.state;
-    this.setState({counter: counter + 1})
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({posts: postsAndPhotos})
   }
 
   render(){
-    const {name, counter} = this.state;
+    const {posts} = this.state;
     return(
-      <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p onClick={this.handlePClick}>
-           {name} {counter}
-         </p>
-         <a
-          onClick= {this.handlAClick}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <section className="container">
+        <Posts posts={posts}/>
+      </section>
+      
     )
   }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
